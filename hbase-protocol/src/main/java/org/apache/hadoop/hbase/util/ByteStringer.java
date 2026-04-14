0e3e870b5f3be04fa6b9fd36e100d06253c35f76
Andrei Shitov <sai@arenadata.io>
2026-04-14 13:35:41 +0300

    chore: remove protobuf 2.5.0

--- hbase-protocol/src/main/java/org/apache/hadoop/hbase/util/ByteStringer.java
+++ hbase-protocol/src/main/java/org/apache/hadoop/hbase/util/ByteStringer.java
@@ -18,35 +18,11 @@
 package org.apache.hadoop.hbase.util;
 
 import com.google.protobuf.ByteString;
-import com.google.protobuf.HBaseZeroCopyByteString;
+import com.google.protobuf.UnsafeByteOperations;
 import org.apache.yetus.audience.InterfaceAudience;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-/**
- * Hack to workaround HBASE-10304 issue that keeps bubbling up when a mapreduce context.
- */
 @InterfaceAudience.Private
 public class ByteStringer {
-  private static final Logger LOG = LoggerFactory.getLogger(ByteStringer.class);
-
-  /**
-   * Flag set at class loading time.
-   */
-  private static boolean USE_ZEROCOPYBYTESTRING = true;
-
-  // Can I classload HBaseZeroCopyByteString without IllegalAccessError?
-  // If we can, use it passing ByteStrings to pb else use native ByteString though more costly
-  // because it makes a copy of the passed in array.
-  static {
-    try {
-      HBaseZeroCopyByteString.wrap(new byte[0]);
-    } catch (IllegalAccessError iae) {
-      USE_ZEROCOPYBYTESTRING = false;
-      LOG.debug("Failed to classload HBaseZeroCopyByteString: " + iae.toString());
-    }
-  }
-
   private ByteStringer() {
     super();
   }
@@ -55,17 +31,13 @@ public class ByteStringer {
    * Wraps a byte array in a {@link ByteString} without copying it.
    */
   public static ByteString wrap(final byte[] array) {
-    return USE_ZEROCOPYBYTESTRING
-      ? HBaseZeroCopyByteString.wrap(array)
-      : ByteString.copyFrom(array);
+    return UnsafeByteOperations.unsafeWrap(array);
   }
 
   /**
    * Wraps a subset of a byte array in a {@link ByteString} without copying it.
    */
   public static ByteString wrap(final byte[] array, int offset, int length) {
-    return USE_ZEROCOPYBYTESTRING
-      ? HBaseZeroCopyByteString.wrap(array, offset, length)
-      : ByteString.copyFrom(array, offset, length);
+    return UnsafeByteOperations.unsafeWrap(array, offset, length);
   }
 }
